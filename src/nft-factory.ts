83606d18071457f2673d07f8ea99a38305db7989
Fsud <gfanle111@gmail.com>
2024-04-30 21:18:22 +0800

    nft subgraph

--- src/nft-factory.ts
+++ src/nft-factory.ts
@@ -1,52 +1,36 @@
 import {
   NFTCreated as NFTCreatedEvent,
-  NFTRegesitered as NFTRegesiteredEvent,
-  OwnershipTransferred as OwnershipTransferredEvent
 } from "../generated/NFTFactory/NFTFactory"
-import {
-  NFTCreated,
-  NFTRegesitered,
-  OwnershipTransferred
-} from "../generated/schema"
+
+import { Transfer as TransferEvent, S2NFT } from "../generated/templates/S2NFT/S2NFT"
+import { S2NFT as S2NFTTemplate } from "../generated/templates"
+import { NFTCreated, TokenInfo } from "../generated/schema"
 
 export function handleNFTCreated(event: NFTCreatedEvent): void {
   let entity = new NFTCreated(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
   entity.nftCA = event.params.nftCA
-
   entity.blockNumber = event.block.number
   entity.blockTimestamp = event.block.timestamp
   entity.transactionHash = event.transaction.hash
-
   entity.save()
+
+  S2NFTTemplate.create(event.params.nftCA);
 }
 
-export function handleNFTRegesitered(event: NFTRegesiteredEvent): void {
-  let entity = new NFTRegesitered(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
+export function handleTransfer(event: TransferEvent): void {
+  let entity = new TokenInfo(
+    event.transaction.hash.concatI32(event.logIndex.toI32()).toString()
   )
-  entity.nftCA = event.params.nftCA
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleOwnershipTransferred(
-  event: OwnershipTransferredEvent
-): void {
-  let entity = new OwnershipTransferred(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  entity.ca = event.address;
+  entity.tokenId = event.params.tokenId;
+  let s2Nft = S2NFT.bind(event.address);
+  entity.tokenURL = s2Nft.tokenURI(event.params.tokenId);
+  entity.name = s2Nft.name()
+  entity.owner = event.params.to;
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+  entity.save();
 }
