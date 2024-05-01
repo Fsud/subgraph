d71c4695738eb4496bbd6b29283eb20703280c02
Fsud <gfanle111@gmail.com>
2024-05-01 09:52:41 +0800

    W3D2 thegraph修复问题

--- src/nft-factory.ts
+++ src/nft-factory.ts
@@ -5,6 +5,7 @@ import {
 import { Transfer as TransferEvent, S2NFT } from "../generated/templates/S2NFT/S2NFT"
 import { S2NFT as S2NFTTemplate } from "../generated/templates"
 import { NFTCreated, TokenInfo } from "../generated/schema"
+import { Address } from "@graphprotocol/graph-ts"
 
 export function handleNFTCreated(event: NFTCreatedEvent): void {
   let entity = new NFTCreated(
@@ -19,10 +20,21 @@ export function handleNFTCreated(event: NFTCreatedEvent): void {
   S2NFTTemplate.create(event.params.nftCA);
 }
 
+/**
+ * 新增或修改tokenInfo
+ */
 export function handleTransfer(event: TransferEvent): void {
-  let entity = new TokenInfo(
-    event.transaction.hash.concatI32(event.logIndex.toI32()).toString()
-  )
+  let entity;
+  if (event.params.from.equals(Address.zero())) {
+    entity = new TokenInfo(
+      event.address.toHexString().concat(event.params.tokenId.toString())
+    )
+  } else {
+    entity = TokenInfo.load(event.address.toHexString().concat(event.params.tokenId.toString()))
+  }
+  if (entity == null) {
+    return;
+  }
   entity.ca = event.address;
   entity.tokenId = event.params.tokenId;
   let s2Nft = S2NFT.bind(event.address);
